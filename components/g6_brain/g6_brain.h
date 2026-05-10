c3720272c2359a13eea7d9ec3d50338a3777ae65
6ummy-Dev <zonaescon@gmail.com>
2026-05-10 14:26:36 -0300

    v1.5.5: Updated g6_brain.h with version define and clean pure math struct

--- components/g6_brain/g6_brain.h
+++ components/g6_brain/g6_brain.h
@@ -4,35 +4,27 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define G6_PARAMS 6  // a f² + b v² + c f v + d f + e v + g
+#define G6_BRAIN_VERSION "1.5.5"
+#define G6_PARAMS 6  // HR(f,v) = a*f² + b*v² + c*f*v + d*f + e*v + g
 
 typedef struct {
-    float theta[G6_PARAMS];
-    float P[G6_PARAMS][G6_PARAMS];
-    float lambda;
-    float best_f, best_v;
-    float best_hr, best_eff;
+    float theta[G6_PARAMS];      // RLS coefficients [a, b, c, d, e, g]
+    float P[G6_PARAMS][G6_PARAMS]; // covariance inverse (uncertainty matrix)
+    float lambda;                // forgetting factor (0.97 typical)
+    float best_f, best_v;        // best observed operating point
+    float best_hr, best_eff;     // best hashrate & efficiency (TH/s / W)
     uint32_t update_count;
     bool auto_tune_enabled;
-    float max_temp_c;
-    float max_error_pct;
-    float min_improvement_pct;
-    // Puzzle Extras state
-    float nonce_opt_start;
-    float dup_predict_prob;
-    uint32_t puzzle_runs;
+    float max_temp_c;            // hard safety limit
+    float max_error_pct;         // max acceptable error rate
+    float min_improvement_pct;   // min % predicted gain to justify retune
+    float model_quality;         // 0-1, higher = more confident in fit
 } G6BrainState;
 
 void g6_brain_init(G6BrainState *brain);
 void g6_brain_update(G6BrainState *brain, float f_mhz, float v_mv, float hr_ths, float power_w, float temp_c, float err_pct);
 bool g6_brain_get_optimal(const G6BrainState *brain, float *out_f, float *out_v, float *out_pred_hr);
 void g6_brain_auto_step(G6BrainState *brain, float current_f, float current_v);
-void g6_brain_reset_model(G6BrainState *brain);
-
-// G6 Puzzle Extras
-void g6_puzzle_nonce_optimize(G6BrainState *brain, float recent_share_rate, float error_trend);
-float g6_puzzle_predict_duplicates(G6BrainState *brain, float nonce_range, float job_difficulty);
-void g6_puzzle_extras_run(G6BrainState *brain);
-void g6_puzzle_on_device_solver_demo(G6BrainState *brain, float target, float *solution);
+void g6_brain_reset(G6BrainState *brain);
 
 #endif
